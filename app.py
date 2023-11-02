03a9989dfe8116972673f2beaf8fefb84696c584
ShutDown <118311269+EmrahPaperotto@users.noreply.github.com>
2023-11-02 16:08:34 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+import pyautogui, time, datetime 
+  
+time.sleep(2) 
+  
+while True: 
+    
+    # to display the time at which the message is sent  
+    print(datetime.datetime.now()) 
+    pyautogui.typewrite("Reminder: SDFJNFDJHODJOHNDFGJNHJNODFHAGJNOIPDFGHJNOPDFRHUJNOPDFAHHUJNOPDRFHNUJOHFDOUJHRFDJNOHJIONKGBHèFDERAJIOHèGDRFHAHJIOUèGDRFHAHUIORFOIUHJAEDRHFGHJUIONOUIPASGNIPUFJDANFY8OIRGBNAERGIHJLBUIGERN")  
+    pyautogui.press("enter") 
+    time.sleep(31) 
+  
+    print(datetime.datetime.now()) 
+  
+    pyautogui.typewrite("Reminder: HAHAHAHHAHAHAHAHAHAHAHAHAHAHHAHAHAHAHHAHAHAHAHAHAHHAHH") 
+    pyautogui.press("enter") 
+    time.sleep(31) 
+  
+    print(datetime.datetime.now()) 
+  
+    pyautogui.typewrite("Reminder: AHHAHAHAHAHAHAHAHHAHAHAHAHAHHAHAHAHAHAHHAHAHAHAHAHAHHA") 
+    pyautogui.press("enter") 
+    time.sleep(31) 
+  
+    print(datetime.datetime.now()) 
+  
+    pyautogui.typewrite("Reminder: HAHHAHAHAHAHAHAHAHHAHAHAHAHAHAHAHHAHAHHAHHAHAHHAHHAGHAGA") 
+    pyautogui.press("enter") 
+    time.sleep(31) 
+  
+    print(datetime.datetime.now()) 
+  
+    pyautogui.typewrite("Reminder: HAHAHAHAHHAHAHAHAHAHAHAHAHHAHAHAHAHAHAHAHAHAHHAHAHAHAHAH!") 
+    pyautogui.press("enter") 
+    time.sleep(31) 
